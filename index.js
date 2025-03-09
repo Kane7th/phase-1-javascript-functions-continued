7d877870c14e620629623d3dd4e0a27b217c5dff
Kane Kabena <onekaneldn@gmail.com>
2025-03-09 08:06:54 +0300

    Functions continued

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(task = "roller-skate") {
+    return (`This Saturday, I want to ${task}!`);
+}
+
+console.log(saturdayFun()); 
+console.log(saturdayFun("bathe my dog"));
+
+//-----------------------------------------------------------------------------------------------------------
+
+const mondayWork = function(task = "go to the office"){
+    return (`This Monday, I will ${task}.`)
+}
+console.log(mondayWork());
+console.log(mondayWork("wash the car"));
+
+//-----------------------------------------------------------------------------------------------------------
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
